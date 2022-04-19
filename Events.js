a1aef13b67fce7d1e76f4ad0f9419621be2ad7f2
ZeroIsDead <zerojonathansutanto@gmail.com>
2022-04-19 14:10:27 +0700

    Arsenal Content; Patch notes button interaction

--- Events.js
+++ Events.js
@@ -1,8 +1,24 @@
 function Show(Version) {
-    var item = document.getElementsByClassName("PN_Content")[Version];
+    let item = document.getElementsByClassName("PN_Content")[Version];
+    let base = document.getElementsByClassName("Show")[Version];
     if (item.style.display == "none") {
         item.style.display = "block";
+        base.style.backgroundColor = "#2525FF";
     } else {
         item.style.display = "none";
+        base.style.backgroundColor = "#000000";
+    }
+}
+var change = true;
+function contentChange(Version) {
+    let imageContent = document.getElementsByClassName("arsenalItem")[Version];
+    let textContent = document.getElementsByClassName("arsenalContent")[Version];
+    if (imageContent.style.display == "none") {
+        imageContent.style.display = "block";
+        textContent.style.display = "none";
+    } else {
+        textContent.style.display = "block";
+        imageContent.style.display = "none";
+
     }
 }
