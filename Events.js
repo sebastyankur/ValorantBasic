180ca8178f7b2ba961759c0b8a47308207df1f5c
ZeroIsDead <zerojonathansutanto@gmail.com>
2022-04-24 13:49:51 +0700

    Dynamic-Button Anchor Jumps

--- Events.js
+++ Events.js
@@ -22,3 +22,28 @@ function contentChange(Version) {
 
     }
 }
+
+
+window.addEventListener('scroll', function() {
+    changeButton("Duelist", "Duelist", "Sentinal");
+    changeButton("Duelist", "Sentinal", "Controller");
+    changeButton("Sentinal", "Controller", "Initiator");
+    changeButton("Controller", "Initiator", "Initiator");
+})
+
+function changeButton(before, now, after) {
+    const a = document.querySelector(`#${before}`).getBoundingClientRect();
+    const b = document.querySelector(`#${now}`).getBoundingClientRect();
+    const c = document.querySelector(`#${after}`).getBoundingClientRect();
+    const d = document.querySelector("#Prev");
+    const e = document.querySelector("#Next");
+    if ((b.top < 250 && c.top > 450) || (a.top < 250 && b.top < 250)) {
+        d.innerHTML = `<a href="#${before}">${before}</a>`;
+        e.innerHTML = `<a href="#${after}">${after}</a>`;
+    }
+}
+
+window.addEventListener('click', function() {
+    const base = document.querySelector(".move");
+    if (base.style.display == "none") {base.style.display = "flex"} else {base.style.display = "none"}
+})
